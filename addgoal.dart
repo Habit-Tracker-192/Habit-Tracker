179064a0ed0ee9d899609df7a8f565b612144784
louisedenise <91036777+louisedenise@users.noreply.github.com>
2022-03-04 07:56:19 +0800

    Add files via upload

---
+++ addgoal.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:app2/Screens/Addgoal/components/body.dart';
+
+class AddGoal extends StatelessWidget {
+  const AddGoal({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: Body());
+  }
+} 
