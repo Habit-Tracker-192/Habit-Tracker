e5f5eb5a71e5e8bc5a0fdfd8694918351803c9e8
zddelacalzada <91069456+zddelacalzada@users.noreply.github.com>
2022-03-18 07:09:58 +0800

    Update main.dart

--- first_app/lib/main.dart
+++ first_app/lib/main.dart
@@ -1,6 +1,6 @@
 // import 'dart:html';
 // import 'package:flutter/services.dart';
-import 'package:first_app/notifications.dart';
+import 'package:first_app/notifs.dart';
 import 'package:first_app/home.dart';
 import 'package:first_app/goals_medyofinal.dart';
 import 'package:first_app/profile.dart';
@@ -35,8 +35,8 @@ class _MyAppState extends State<MyApp> {
   final screens = [
     HomePage(),
     Goal(),
-    NotificationPage(),
-    ProfilePage(),
+    Notif(),
+    Profile(),
   ];
   
   final frequencies = [
@@ -91,196 +91,7 @@ class _MyAppState extends State<MyApp> {
           ],
           ),
         ),
-        body: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-
-                TextFormField(
-                  key: _textformfield_goalName,
-                  controller: _goalNameController,
-                  decoration: const InputDecoration(
-                    hintText: 'Your Goal Name',
-                    labelText: 'Goal Name',
-                    labelStyle: TextStyle(
-                      fontSize: 18,
-                      color:  Color.fromRGBO(100, 88, 204, 1),
-                    ),
-                    border: OutlineInputBorder(),
-                    fillColor: Color.fromARGB(255 ,221,223,245),
-                    filled: true,
-                  ),
-                  textInputAction: TextInputAction.done,
-                  validator: (value){
-                    if (value!.isNotEmpty && value.length <= 24 ){
-                      return null;
-                    }
-                    else if (value.isEmpty){
-                      return 'Please add a goal name';
-                    } 
-                    else {
-                      return 'Goal Name is too long!';
-                    }
-                  },
-                ),
-
-                TextFormField(   
-                  decoration: const InputDecoration(
-                    hintText: 'Which one of your Categories does this Goal belong?',
-                    labelText: 'Category',
-                    labelStyle: TextStyle(
-                      fontSize: 18,
-                      color:  Color.fromRGBO(100, 88, 204, 1),
-                    ),
-                    border: OutlineInputBorder(),
-                    fillColor: Color.fromARGB(255 ,221,223,245),
-                    filled: true,
-                  ),
-                ),
-
-                DropdownButtonFormField<String>(
-                  value: value,
-                  key: _textformfield_frequency,
-                  dropdownColor: Color.fromARGB(255 ,221,223,245),
-                  isExpanded: true,
-                  items: frequencies.map(buildMenuItem).toList(),
-                  onChanged: (value) => setState(() => frequency = value!),
-                  iconEnabledColor:  const Color.fromRGBO(100, 88, 204, 1),
-                  style: const TextStyle(
-                    color:  Color.fromRGBO(100, 88, 204, 1),
-                  ),
-                  decoration: const InputDecoration(
-                    hintText: 'How often would you like to achieve your goal?',
-                    labelText: 'Frequency',
-                    labelStyle: TextStyle(
-                      fontSize: 18,
-                      color:  Color.fromRGBO(100, 88, 204, 1),
-                    ),
-                    border: OutlineInputBorder(),
-                    fillColor: Color.fromARGB(255 ,221,223,245),
-                    filled: true,
-                  ),
-                  validator: (value){
-                    if (value!.isEmpty){
-                      return 'you must choose a frequency';
-                    }
-                    else{
-                      return null;
-                    }
-                  },
-                ),
-
-                TextFormField(
-                  // onChanged: (value) =>setState(() => this.);,\
-                  key: _textformfield_times,
-                  controller: _timesController,
-                  decoration: const InputDecoration(
-                    hintText: 'Adjust if you want more than once a day/week/month',
-                    labelText: 'Times',
-                    labelStyle: TextStyle(
-                      fontSize: 18,
-                      color:  Color.fromRGBO(100, 88, 204, 1),
-                    ),
-                    border: OutlineInputBorder(),
-                    fillColor: Color.fromARGB(255 ,221,223,245),
-                    filled: true,
-                    // errorText: _timesError,
-                  ),
-                  keyboardType: TextInputType.number,
-                  validator: (val){
-                    if (val!.isEmpty){
-                      return 'Please put your desired number of repetition/s';
-                    }
-
-                    else if (frequency == 'Daily' && double.tryParse(val!)!<= 24 ){
-                      return null;
-                    }
-                    
-                    else if (frequency == 'Weekly' && double.tryParse(val!)!<= 6 ){
-                      return null;
-                    }
-
-                    else if (frequency == 'Monthly' && double.tryParse(val!)!<= 30 ){
-                      return null;
-                    }
-
-                    else {
-                      return 'You have exceeded the maximum amount of repetitions';
-                    } 
-                  },
-                ),
-
-                TextFormField(
-                  key: _textformfield_duration,
-                  controller: _durationController,
-                  decoration: const InputDecoration(
-                    hintText: 'How many hours would it take?',
-                    labelText: 'Duration',
-                    labelStyle: TextStyle(
-                      fontSize: 18,
-                      color:  Color.fromRGBO(100, 88, 204, 1),
-                    ),
-                    border: OutlineInputBorder(),
-                    fillColor: Color.fromARGB(255 ,221,223,245),
-                    filled: true,
-                  ),
-                  keyboardType: TextInputType.number,
-                  validator: (value){
-                    if (value!.isEmpty){
-                      return 'Please enter duration';
-                    }
-                    else if (frequency == 'Daily' && (int.tryParse(value!)!*int.tryParse(_timesController.text)!)  > 24){
-                      return 'Duration exceeded number of hours per day!';
-                    }
-                    else if (frequency == 'Weekly' && (int.tryParse(value!)!*int.tryParse(_timesController.text)!) > 168){
-                      return 'Duration exceeded number of hours per week!';
-                    }
-                    else if (frequency == 'Monthly' && (int.tryParse(value)!* int.tryParse(_timesController.text)!) > 744){
-                      return 'Duration exceeded number of hours per month!';
-                    }
-                  },
-                ),
-
-                TextFormField(
-                  key: _textformfield_goalDescription,
-                  controller: _goalDescriptionController,
-                  decoration: const InputDecoration(
-                    hintText: 'Detailed description of your goal.',
-                    labelText: 'Description',
-                    labelStyle: TextStyle(
-                      fontSize: 18,
-                      color:  Color.fromRGBO(100, 88, 204, 1),
-                    ),
-                    enabledBorder: OutlineInputBorder(),
-                    fillColor: Color.fromARGB(255 ,221,223,245),
-                    filled: true,
-                  ),
-                  validator: (value){
-                    if (value!.isEmpty){
-                      return 'Please add a goal description';
-                    }
-                    else if (value!.isNotEmpty && value.length <= 99 ){
-                      return null;
-                    } 
-                    else {
-                      return 'Goal description is too long!';
-                    }
-                  },
-                ),
-                
-                RaisedButton(
-                  onPressed: (){
-                    if (!_textformfield_goalName.currentState!.validate()){}
-                    if (!_textformfield_frequency.currentState!.validate()){}
-                    if (!_textformfield_times.currentState!.validate()){}
-                    if (!_textformfield_duration.currentState!.validate()){}
-                    if (!_textformfield_goalDescription.currentState!.validate()){}
-                  },    
-                  child: const Text('Add'),
-                  textColor: Colors.white,
-                  color: const Color.fromRGBO(100, 88, 204, 1),
-                ),
-              ],
-            ),
+        body: screens[currentIndex]
 
         bottomNavigationBar: SizedBox(
           height: 75,
