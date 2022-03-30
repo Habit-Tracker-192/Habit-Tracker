c5f8aa7bae95d97793df1abebf96e83c7832814f
calebbun <90824219+calebbun@users.noreply.github.com>
2022-03-30 16:36:34 +0800

    Update main.dart
    main.dart file now calls Login screen. MyHomePage navigation view is called inside Login screen instead.

--- first_app/lib/main.dart
+++ first_app/lib/main.dart
@@ -1,11 +1,10 @@
 // ignore_for_file: prefer_const_constructors
-
+//packages
 import 'package:flutter/material.dart';
-import 'screens/goals.dart';
-import 'screens/notifs.dart';
-import 'screens/addgoal.dart';
-import 'screens/profile.dart';
-import 'screens/home.dart';
+// import 'package:firebase_core/firebase_core.dart';
+
+//Screens
+import 'screens/login.dart';
 
 
 void main() {
@@ -24,101 +23,12 @@ class MyApp extends StatelessWidget {
         fontFamily: 'Poppins',
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
-
       ),
-      home: MyHomePage(title: '',),
+      home: Login(), 
+      // MyHomePage(title: '',),
+      
       debugShowCheckedModeBanner: false,
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-
-  int _currentIndex = 0;
-
-   final screens = [
-    HomePage(),
-    Goal(),
-    Notif(),
-    Profile(),
-    AddGoal(),
-  ];
-  
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      //appBar: AppBar(
-        //title: Text(widget.title),
-      //),
-      body: screens[_currentIndex],
-      bottomNavigationBar: SizedBox(
-          height: 85,
-          child: BottomNavigationBar(
-            type: BottomNavigationBarType.fixed,
-            backgroundColor: Colors.white,
-            selectedItemColor: const Color.fromRGBO(100, 88, 204, 1),
-            unselectedItemColor: Colors.grey,
-            showUnselectedLabels: false,
-            showSelectedLabels: false,
-            onTap: onTabTapped,
-            currentIndex: _currentIndex,
-            items: const <BottomNavigationBarItem>[
-              BottomNavigationBarItem(
-                icon: Icon(Icons.home_rounded,size: 45),
-                label: 'Home',
-              ),
-              BottomNavigationBarItem(
-                icon: Icon(Icons.description_outlined, size: 45),
-                label: 'Goals',
-              ),
-              BottomNavigationBarItem(
-                icon: Icon(Icons.email_outlined, size: 45),
-                label: 'Notifications',
-              ),
-              BottomNavigationBarItem(
-                icon: Icon(Icons.person_outline_rounded, size: 45),
-                label: 'Profile',
-              ),
-            ],
-            // onTap: (index) {
-            //   setState(() {
-            //     _currentIndex = index;
-            //   });
-            // },
-            elevation: 0, 
-          ),
-        ),
-          floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-          floatingActionButton: FloatingActionButton(
-            onPressed: (){
-              // Navigator.push(
-              // context,
-              // MaterialPageRoute(builder: (context) => const AddGoal()),
-              Navigator.of(context).push(MaterialPageRoute(builder: (context) => AddGoal()),
-
-            );
-                //onTabTapped(3);
-            }, 
-            tooltip: 'Add',
-            child: const Icon(Icons.add),
-            backgroundColor: Color.fromRGBO(100, 88, 204, 1),
-          ),
-      
-    );
-  }
-
-
-  void onTabTapped(int index) {
-    setState(() {
-      _currentIndex = index;
-    });
-  }
-}
