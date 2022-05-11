f58cc5743056def2020086eb2706d3cdaea42179
calebbun <90824219+calebbun@users.noreply.github.com>
2022-05-11 18:50:03 +0800

    Update categList.dart
    included other category info

--- first_app/lib/models/categList.dart
+++ first_app/lib/models/categList.dart
@@ -4,15 +4,25 @@ class CategoryEntity {
   String? category;
   late num categProgress;
   late num categTotal;
+  late num categTargetHours;
+  String? categDesc;
   
 
   CategoryEntity();
 
-  Map<String, dynamic> toJson() => {'category': category, 'categProgress': categProgress,'categTotal': categTotal};
+  Map<String, dynamic> toJson() => {
+    'category': category,
+    'categProgress': categProgress,
+    'categTotal': categTotal,
+    'categTargetHours': categTargetHours,
+    'categDesc': categDesc
+    };
 
   CategoryEntity.fromSnapshot(snapshot)
     : category = snapshot.data()['category'],
       categProgress = snapshot.data()['categProgress'],
-      categTotal = snapshot.data()['categTotal']
+      categTotal = snapshot.data()['categTotal'],
+      categTargetHours = snapshot.data()['categTargetHours'],
+      categDesc = snapshot.data()['categDesc']
       ;
 }
