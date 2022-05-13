72536f748423af89e5f575a18cff11cdeb6884f4
zddelacalzada <91069456+zddelacalzada@users.noreply.github.com>
2022-05-13 19:39:08 +0800

    Update home.dart

--- first_app/lib/screens/home.dart
+++ first_app/lib/screens/home.dart
@@ -1,5 +1,12 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:percent_indicator/circular_percent_indicator.dart';
+import 'package:intl/intl.dart';
+import '../components/goal_card2.dart';
+import 'package:first_app/models/goalList.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:first_app/services/authenticate.dart';
+import 'package:date_format/date_format.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -8,6 +15,17 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  //DateTime dateTime = DateTime.now();
+  DateTime currentTime = DateTime.now();
+  //String date = formatDate(dateTime,[yyyy, '/', mm, '/', dd] );
+  //final DateTime now = DateTime.now();
+  //final DateFormat formatter = DateFormat('yyyy-MM-dd');
+  //final String formatted = formatter.format(now);
+
+  //print(dateTime.toString()); //2022-03-04 12:35:38.997
+ // int currentYear = dateTime.year;
+  //print(currentYear.toString()); //2022
+
   @override
   Widget build(BuildContext context) => DefaultTabController (
       length: 3,
@@ -19,17 +37,18 @@ class _HomePageState extends State<HomePage> {
               flexibleSpace: Container(
                 decoration: const BoxDecoration(
                   image: DecorationImage(
-                    image: AssetImage('assets/images/header.png'),
+                    image: AssetImage('assets/images/design_2.png'),
                     fit: BoxFit.cover
 
                   ),
                 ),
 
                 child: Container(
+
                   margin: EdgeInsets.all(2),
-                  padding: EdgeInsets.fromLTRB(1.0, 100.0, .0, 110.0),
+                  padding: EdgeInsets.fromLTRB(1.0, 110.0, .0, 100.0),
                   width: 500,
-                  child: const Text('FRIDAY, APRIL 1',
+                  child: Text(DateFormat.yMd().format(currentTime),
                     textAlign: TextAlign.center,
                     style: TextStyle(
                         fontFamily: 'Poppins',
@@ -68,70 +87,122 @@ class _HomePageState extends State<HomePage> {
           )),
           body: TabBarView(
             children: [
-              Recent(),
-              Recent(),
-              Recent(),
+              Daily(),
+              Weekly(),
+              Monthly(),
             ],)
       )
   );
 }
 
-class Recent extends StatefulWidget {
-  _RecentState createState() => _RecentState();
+class Daily extends StatefulWidget {
+  _DailyState createState() => _DailyState();
 }
+class _DailyState extends State<Daily> {
+  List<Object> _goals = [];
+  @override
+  void didChangeDependencies(){
+    super.didChangeDependencies();
+    getGoalList();
+  }
 
-class _RecentState extends State<Recent> {
+  Future getGoalList() async {
+    final uid = FireAuth().currentUser?.uid;
 
-  List goal = ["Journaling", "Study CS 191", "Rewrite Notes", "Exercise", "Read Books"];
-  List category = ["Recreation", "Education", "Education", "Health", "Recreation"];
-  List progress = [20, 35, 15, 35, 54];
-  List total = [20, 50, 20, 50, 90];
-
+    var data = await FirebaseFirestore.instance
+        .collection('UserData')
+        .doc(uid)
+        .collection('goals')
+        .where('frequency', isEqualTo: "Daily")
+        .get();
+    setState(() {
+      _goals = List.from(data.docs.map((doc)=> GoalEntity.fromSnapshot(doc)));
+    });
+  }
 
   @override
   Widget build(BuildContext context) => Scaffold(
       body: GridView.builder(
-        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 2),
-        itemCount: goal.length,
-        shrinkWrap: true,
-        itemBuilder: (BuildContext context, int index) => Container(
-            width: MediaQuery.of(context).size.width,
-            padding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 20.0),
-
-              child: Container(
-                  width: MediaQuery.of(context).size.width,
-                  //padding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 10.0),
-                  decoration: const BoxDecoration(
-                    shape: BoxShape.circle,
-                    boxShadow: [
-                      BoxShadow(
-                        color:  Color.fromRGBO(255, 255, 255, .5),
-                      ),
-                    ],
-                  ),
-                        child: CircularPercentIndicator(
-                                radius: 150,
-                                animation: true,
-                                //strokeWidth: 10,
-                                lineWidth: 10,
-
-                                center: Text(goal[index], style: const TextStyle(color:
-                                  Color.fromARGB(255, 72, 68, 80), 
-                                  fontSize:16.0, 
-                                  fontFamily: 'Poppins',
-                                  fontWeight: FontWeight.bold, 
-                                  letterSpacing: 1.0), 
-                                textAlign: TextAlign.justify,),
-                                circularStrokeCap: CircularStrokeCap.round,
-                                percent: progress[index]/total[index],
-                                progressColor: Color.fromARGB(255, 111, 104, 207),
-                                backgroundColor: Color.fromRGBO(201, 196, 246, 1.0),
-                                // 255, 111, 104, 207
-                        )
-              )
-        ),
-
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2),
+          itemCount: _goals.length,
+          shrinkWrap: true,
+          itemBuilder: (BuildContext context, int index) => GoalCard2(_goals[index] as GoalEntity)
       )
   );
 }
+
+class Weekly extends StatefulWidget {
+  _WeeklyState createState() => _WeeklyState();
+}
+class _WeeklyState extends State<Weekly> {
+  List<Object> _goals = [];
+  @override
+  void didChangeDependencies(){
+    super.didChangeDependencies();
+    getGoalList();
+  }
+
+  Future getGoalList() async {
+    final uid = FireAuth().currentUser?.uid;
+
+    var data = await FirebaseFirestore.instance
+        .collection('UserData')
+        .doc(uid)
+        .collection('goals')
+        .where('frequency', isEqualTo: "Weekly")
+        .get();
+    setState(() {
+      _goals = List.from(data.docs.map((doc)=> GoalEntity.fromSnapshot(doc)));
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) => Scaffold(
+      body: GridView.builder(
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2),
+          itemCount: _goals.length,
+          shrinkWrap: true,
+          itemBuilder: (BuildContext context, int index) => GoalCard2(_goals[index] as GoalEntity)
+      )
+  );
+}
+
+class Monthly extends StatefulWidget {
+  _MonthlyState createState() => _MonthlyState();
+}
+class _MonthlyState extends State<Monthly> {
+  List<Object> _goals = [];
+  @override
+  void didChangeDependencies(){
+    super.didChangeDependencies();
+    getGoalList();
+  }
+
+  Future getGoalList() async {
+    final uid = FireAuth().currentUser?.uid;
+
+    var data = await FirebaseFirestore.instance
+        .collection('UserData')
+        .doc(uid)
+        .collection('goals')
+        .where('frequency', isEqualTo: "Monthly")
+        .get();
+    setState(() {
+      _goals = List.from(data.docs.map((doc)=> GoalEntity.fromSnapshot(doc)));
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) => Scaffold(
+      body: GridView.builder(
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2),
+          itemCount: _goals.length,
+          shrinkWrap: true,
+          itemBuilder: (BuildContext context, int index) => GoalCard2(_goals[index] as GoalEntity)
+      )
+  );
+}
+
