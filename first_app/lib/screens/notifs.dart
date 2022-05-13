ed7727240794184fad0f6d4dbf72a2ef13b2cf92
calebbun <90824219+calebbun@users.noreply.github.com>
2022-05-13 22:39:10 +0800

    Update notifs.dart

--- first_app/lib/screens/notifs.dart
+++ first_app/lib/screens/notifs.dart
@@ -1,8 +1,11 @@
-// ignore_for_file: prefer_const_constructors
-
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 import 'package:flutter/material.dart';
-import 'package:percent_indicator/percent_indicator.dart';
-import 'searchUser.dart';
+import 'package:first_app/components/notif_card.dart';
+import 'package:first_app/models/notification.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:first_app/services/authenticate.dart';
+import 'package:first_app/screens/searchUser.dart';
 
 
 class Notif extends StatefulWidget {
@@ -10,10 +13,10 @@ class Notif extends StatefulWidget {
 
 
   @override
-  _NotifState createState() => _NotifState();
+  _NotificationPageState createState() => _NotificationPageState();
 }
 
-class _NotifState extends State<Notif> {
+class _NotificationPageState extends State<Notif> {
   @override
   Widget build(BuildContext context) => DefaultTabController (
       length: 1,
@@ -25,7 +28,7 @@ class _NotifState extends State<Notif> {
               title: Container(
                 child: IconButton(
                   alignment: Alignment.topRight,
-                  padding: EdgeInsets.fromLTRB(330, 15, 0, 0),
+                  padding: EdgeInsets.fromLTRB(350, 15, 0, 0),
                   icon: Icon(Icons.search_rounded, size: 30.0),
 
                   onPressed: (){
@@ -48,113 +51,50 @@ class _NotifState extends State<Notif> {
           )),
           body: TabBarView(
             children: [
-              Recent()
-              //Completed(),
-              //All(),
+              Notifs()
             ],)
       )
   );
 }
-class Recent extends StatefulWidget {
-  _RecentState createState() => _RecentState();
+
+class Notifs extends StatefulWidget {
+  _NotifState createState() => _NotifState();
 }
 
-class _RecentState extends State<Recent> {
+class _NotifState extends State<Notifs> {
 
-  List goal = ["@CalebBunye accepted your friend request.", "@DenCute sent you a friend request.", "Take / Fix Notes", "Exercise", "Read Books"];
+  // List goal = ["@CalebBunye accepted your friend request.", "@DenCute sent you a friend request.", "Take / Fix Notes", "Exercise", "Read Books"];
 
-  List progress = [20, 35, 15, 35, 54];
-  List total = [20, 50, 20, 50, 90];
+  // List progress = [20, 35, 15, 35, 54];
+  // List total = [20, 50, 20, 50, 90];
+  List<Object> _notif = [];
 
+  @override
+  void didChangeDependencies(){
+    super.didChangeDependencies();
+    getNotifList();
+  }
+
+  Future getNotifList() async {
+    final uid = FireAuth().currentUser?.uid;
+    var data = await FirebaseFirestore.instance
+      .collection('UserData')
+      .doc(uid)
+      .collection('notifications')
+      .get();
+    setState(() {
+      _notif = List.from(data.docs.map((doc)=> NotificationEntity.fromSnapshot(doc)));
+    });
+    print(_notif);
+  }
 
   @override
   Widget build(BuildContext context) => Scaffold(
       body: ListView.builder(
-        itemCount: 2,
+        padding: EdgeInsets.fromLTRB(0, 20, 0, 0),
+        itemCount: _notif.length,
         shrinkWrap: true,
-        itemBuilder: (BuildContext context, int index) => Container(
-            width: MediaQuery.of(context).size.width,
-            padding: EdgeInsets.symmetric(horizontal: 5.0, vertical: 5.0),
-            child: Card(
-              elevation: 5.0,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(15.0),
-              ),
-              child: Container(
-                  //width: MediaQuery.of(context).size.width,
-                  padding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 10.0),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Row(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: <Widget>[
-                          //SizedBox(width: 5.0),
-                          Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            //mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                            children: <Widget>[
-                              Row(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: <Widget>[
-                                IconButton(
-                                  alignment: Alignment.topLeft,
-                                  padding: EdgeInsets.fromLTRB(0, 10, 0, 0),
-                                  icon: Icon(Icons.person, size: 50.0, color: const Color.fromRGBO(100, 88, 204, .9)),
-                                  onPressed: (){},
-                                ),
-
-                                Container(
-                                  width: 300,
-                                  padding: EdgeInsets.symmetric(horizontal: 00.0, vertical: 10.0),
-                                    child: Text(
-                                      goal[index], style: TextStyle(color: Color.fromARGB(255, 72, 68, 80), fontSize: 18.0, fontFamily: 'Poppins', fontWeight: FontWeight.bold, letterSpacing: 1.1), textAlign: TextAlign.left,
-                                      overflow: TextOverflow.ellipsis,
-                                      maxLines: 2,),
-                                ),
-                                ]
-                              ),
-
-
-                              Row(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: <Widget>[
-                                  Container(
-                                    // width: MediaQuery.of(context).size.width,
-                                    padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 20.0),
-                                    child: Text(
-                                      "2h", style: TextStyle(color: Color.fromARGB(255, 72, 68, 80), fontSize: 12.0, fontFamily: 'Poppins', fontWeight: FontWeight.normal), textAlign: TextAlign.left,),
-                                  ),
-                                  IconButton(
-                                    alignment: Alignment.topRight,
-                                    padding: EdgeInsets.fromLTRB(220, 10, 0, 0),
-                                    icon: Icon(Icons.check_rounded, size: 25.0),
-
-                                    onPressed: (){},
-                                  ),
-                                  IconButton(
-                                    alignment: Alignment.topRight,
-                                    padding: EdgeInsets.fromLTRB(0, 10, 0, 0),
-                                    icon: Icon(Icons.clear_rounded, size: 25.0),
-
-                                    onPressed: (){},
-                                  )
-                                ]
-                              ),
-
-                            ],
-                          ),
-
-
-
-                            ],
-                          ),
-                        ],
-                      )
-                    ),
-                  )
-              ),
+        itemBuilder: (BuildContext context, int index) =>NotifCard(_notif[index] as NotificationEntity)
             )
         );
 }
