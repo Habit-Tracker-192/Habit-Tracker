de5d8d091795548d2773c0f9e84f94b7b9fb2864
louisedenise <91036777+louisedenise@users.noreply.github.com>
2022-05-13 21:32:49 +0800

    Update alert_dialog_cat.dart

--- first_app/lib/components/alert_dialog_cat.dart
+++ first_app/lib/components/alert_dialog_cat.dart
@@ -1,20 +1,23 @@
 // ignore_for_file: prefer_const_constructors, deprecated_member_use
 
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:first_app/models/goalList.dart';
 import 'package:first_app/services/authenticate.dart';
+//import 'package:first_app/components/alert_dialog_deleteGoal.dart';
 import 'package:flutter/material.dart';
 
 enum DialogsAction { yes, cancel}
 
 class AlertDialogsCat {
 
-  //DocumentReference reference = FirebaseFirestore.instance.reference();
   static Future<DialogsAction> yesCancelDialog(
     BuildContext context,
     String category,
+    String hasGoal,
     String title,
     String body,
   ) async {
+    
     final action = await showDialog(
       context: context,
       barrierDismissible: false,
@@ -27,13 +30,62 @@ class AlertDialogsCat {
             RaisedButton(
               shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
               onPressed: () async { 
-                  //String? data = _goal.goal;
-                  final uid = FireAuth().currentUser?.uid;
-                  (category != "Instance of 'CategoryEntity") ?
-                  await FirebaseFirestore.instance.collection('UserData').doc(uid).collection('categories').doc(category).delete().whenComplete(() {
-                        print("$category deleted"); 
-                        }): print("category delete");
-                  Navigator.of(context).pop(DialogsAction.yes);},
+                  
+                  final uid = FireAuth().currentUser?.uid;   
+                  final snapshot = await FirebaseFirestore.instance.collection('UserData').doc(uid).collection('categories').doc(category).collection('goals').limit(1).get();
+
+                  // (hasGoal != 'true') ?
+                  // await FirebaseFirestore.instance.collection('UserData').doc(uid).collection('categories').doc(category).delete().whenComplete(() {
+                  //       //print("$category deleted "); 
+                  //       Navigator.of(context).pop(DialogsAction.yes);
+                  //       showDialog(
+                  //       context: context,
+                  //       builder: (BuildContext context) => _buildPopupDialogDeleteCategory(context) ,
+                  //         ); 
+                  //       }): 
+                  //       Navigator.of(context).pop(DialogsAction.yes);
+                  //       showDialog(
+                  //       context: context,
+                  //       builder: (BuildContext context) => _buildPopupDialogCannotDeleteCategory(context)
+                  //        ,
+                  //         ); 
+                  if(hasGoal != 'true'){
+                    await FirebaseFirestore.instance.collection('UserData').doc(uid).collection('categories').doc(category).delete().whenComplete(() {
+                        print("$category deleted "); 
+                        Navigator.of(context).pop(DialogsAction.yes);
+                        showDialog(
+                        context: context,
+                        builder: (BuildContext context) => _buildPopupDialogDeleteCategory(context) ,
+                          ); 
+                        });
+                  } else {
+                    print("Category contains goals!");
+                    Navigator.of(context).pop(DialogsAction.yes);
+                        showDialog(
+                        context: context,
+                        builder: (BuildContext context) => _buildPopupDialogCannotDeleteCategory(context));
+                  }
+                  // (hasGoal != 'true') ?
+                  // await FirebaseFirestore.instance.collection('UserData').doc(uid).collection('categories').doc(category).delete().whenComplete(() {
+                  //       //print("$category deleted "); 
+                  //       Navigator.of(context).pop(DialogsAction.yes);
+                  //       showDialog(
+                  //       context: context,
+                  //       builder: (BuildContext context) => _buildPopupDialogDeleteCategory(context) ,
+                  //         ); 
+                  //       }): 
+                  //       Navigator.of(context).pop(DialogsAction.yes);
+                  //       showDialog(
+                  //       context: context,
+                  //       builder: (BuildContext context) => _buildPopupDialogCannotDeleteCategory(context)
+                  //        ,
+                  //         ); 
+
+                          
+                        //final action = await AlertDialogsCat.yesCancelDialog(context, _category.category.toString(), _category.hasGoal.toString(), 'Delete Category', 'Are you sure you want to delete ');
+                  
+                  //Navigator.of(context).pop(DialogsAction.yes);
+                  },
               child: Text(
                 'Confirm', style: TextStyle(color: Color.fromARGB(255, 121, 38, 216), fontWeight: FontWeight.bold),
               ),
@@ -51,5 +103,45 @@ class AlertDialogsCat {
       }
     );
     return (action != null) ? action : DialogsAction.cancel;
+  }
+
+  static _buildPopupDialogDeleteCategory(BuildContext context) {
+    return AlertDialog(
+    title: const Text('Successfully deleted a Category!'),
+    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+    content:  Column(
+      mainAxisSize: MainAxisSize.min,
+      crossAxisAlignment: CrossAxisAlignment.start,
+    ),
+    actions: <Widget>[
+       FlatButton(
+        onPressed: () {
+          Navigator.of(context).pop();
+        },
+        textColor: Theme.of(context).primaryColor,
+        child: const Text('Close'),
+      ),
+    ],
+  );
+  }
+
+  static _buildPopupDialogCannotDeleteCategory(BuildContext context) {
+    return AlertDialog(
+    title: const Text('Category contains goals!'),
+    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+    content:  Column(
+      mainAxisSize: MainAxisSize.min,
+      crossAxisAlignment: CrossAxisAlignment.start,
+    ),
+    actions: <Widget>[
+       FlatButton(
+        onPressed: () {
+          Navigator.of(context).pop();
+        },
+        textColor: Theme.of(context).primaryColor,
+        child: const Text('Close'),
+      ),
+    ],
+  );
   }
 }
