afedc86975e4e2e5283ca443eef6548b3c2ed9f1
calebbun <90824219+calebbun@users.noreply.github.com>
2022-05-13 22:44:11 +0800

    Update goalList.dart

--- first_app/lib/models/goalList.dart
+++ first_app/lib/models/goalList.dart
@@ -1,4 +1,5 @@
 import 'dart:ffi';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class GoalEntity {
   String? goal;
@@ -7,6 +8,7 @@ class GoalEntity {
   late num total;//times
   late num duration;
   String? desc;
+  late Timestamp lastlog;
   
   late num percent;
   late num progress;
@@ -24,6 +26,7 @@ class GoalEntity {
       frequency = snapshot.data()['frequency'],
       total = snapshot.data()['total'],
       duration = snapshot.data()['duration'],
-      desc = snapshot.data()['desc']
+      desc = snapshot.data()['desc'],
+      lastlog = snapshot.data()['lastlog']
       ;
 }
