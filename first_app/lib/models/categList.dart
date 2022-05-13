a150e58a4e9a8c76473254c7667ea9731c6e33b8
louisedenise <91036777+louisedenise@users.noreply.github.com>
2022-05-13 21:38:41 +0800

    Update categList.dart

--- first_app/lib/models/categList.dart
+++ first_app/lib/models/categList.dart
@@ -1,3 +1,22 @@
+// import 'dart:ffi';
+
+// class CategoryEntity {
+//   String? category;
+//   late num categProgress;
+//   late num categTotal;
+  
+
+//   CategoryEntity();
+
+//   Map<String, dynamic> toJson() => {'category': category, 'categProgress': categProgress,'categTotal': categTotal};
+
+//   CategoryEntity.fromSnapshot(snapshot)
+//     : category = snapshot.data()['category'],
+//       categProgress = snapshot.data()['categProgress'],
+//       categTotal = snapshot.data()['categTotal']
+//       ;
+// }
+
 import 'dart:ffi';
 
 class CategoryEntity {
@@ -6,6 +25,8 @@ class CategoryEntity {
   late num categTotal;
   late num categTargetHours;
   String? categDesc;
+  late bool hasGoal;
+  //late num goalCount;
   
 
   CategoryEntity();
@@ -15,7 +36,9 @@ class CategoryEntity {
     'categProgress': categProgress,
     'categTotal': categTotal,
     'categTargetHours': categTargetHours,
-    'categDesc': categDesc
+    'categDesc': categDesc,
+    'hasGoal': hasGoal,
+    //'goalCount': goalCount
     };
 
   CategoryEntity.fromSnapshot(snapshot)
@@ -23,6 +46,8 @@ class CategoryEntity {
       categProgress = snapshot.data()['categProgress'],
       categTotal = snapshot.data()['categTotal'],
       categTargetHours = snapshot.data()['categTargetHours'],
-      categDesc = snapshot.data()['categDesc']
+      categDesc = snapshot.data()['categDesc'],
+      hasGoal = snapshot.data()['hasGoal']
+      //goalCount = snapshot.data()['goalCount']
       ;
 }
