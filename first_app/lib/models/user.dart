ed7d775c19e7997621a2e45849c8d1cc49d07b08
calebbun <90824219+calebbun@users.noreply.github.com>
2022-05-23 17:03:10 +0800

    Update user.dart
    added lastlogin property

--- first_app/lib/models/user.dart
+++ first_app/lib/models/user.dart
@@ -1,5 +1,7 @@
 import 'dart:ffi';
 
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 class UserEntity {
   String? name;
   String? email;
@@ -7,6 +9,7 @@ class UserEntity {
   String? gender;
   List<Object>? friends;
   String? bio;
+  late Timestamp lastlogin;
 
   
   // late num percent;
@@ -15,7 +18,7 @@ class UserEntity {
 
   UserEntity();
 
-  Map<String, dynamic> toJson() => {'name': name, 'email': email,'age': age, 'gender': gender, 'friends': friends, 'bio': bio};
+  Map<String, dynamic> toJson() => {'name': name, 'email': email,'age': age, 'gender': gender, 'friends': friends, 'bio': bio, 'lastlogin': lastlogin};
 
   UserEntity.fromSnapshot(snapshot): 
       name = snapshot.data()['name'],
@@ -23,5 +26,6 @@ class UserEntity {
       age = snapshot.data()['age'], 
       gender = snapshot.data()['gender'],
       friends = snapshot.data()['friends'],
-      bio = snapshot.data()['bio'];
+      bio = snapshot.data()['bio'],
+      lastlogin = snapshot.data()['lastlogin'];
 }
